5a7ff22252f5b2c939cf9b3a6fe0d06445496571
Alex Brandt <alunduil@alunduil.com>
2014-09-25 12:47:19 -0500

    Split overlay tests into unit and functional suites.

--- test_etest/test_unit/test_overlay.py
+++ test_etest/test_unit/test_overlay.py
@@ -10,8 +10,6 @@ import tempfile
 
 from etest import overlay
 
-from test_etest.test_fixtures import FIXTURES_DIRECTORY
-
 
 class TestOverlayWithInvalidOverlay(unittest.TestCase):
     def test_invalid_overlay(self):
@@ -60,23 +58,3 @@ class TestOverlayWithEmptyOverlay(unittest.TestCase):
         self.overlay = overlay.Overlay()
 
         self.assertEqual(0, len(self.overlay.ebuilds))
-
-
-class TestOverlayWithNonEmptyOverlay(unittest.TestCase):
-    def setUp(self):
-        self.addCleanup(functools.partial(os.chdir, os.getcwd()))
-        os.chdir(os.path.join(FIXTURES_DIRECTORY, 'overlay'))
-
-    def test_nonempty_overlay_directory_detection(self):
-        '''overlay.Overlay().directory—nonempty overlay'''
-
-        self.overlay = overlay.Overlay()
-
-        self.assertEqual(os.path.join(FIXTURES_DIRECTORY, 'overlay'), self.overlay.directory)
-
-    def test_nonempty_overlay_nonempty_ebuilds(self):
-        '''len(overlay.Overlay().ebuilds) != 0—nonempty overlay'''
-
-        self.overlay = overlay.Overlay()
-
-        self.assertEqual(1, len(self.overlay.ebuilds))
