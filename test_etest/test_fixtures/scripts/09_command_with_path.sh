6c89492955d6440d6d7127a04bac059c975422dd
Alex Brandt <alunduil@alunduil.com>
2014-11-06 21:16:12 -0600

    Add path handling to word lexing.

--- test_etest/test_fixtures/scripts/09_command_with_path.sh
+++ test_etest/test_fixtures/scripts/09_command_with_path.sh
@@ -2,12 +2,4 @@ python_install_all() {
         distutils-r1_python_install_all
 
         keepdir /etc/holland
-        keepdir /etc/holland/backupsets
-        keepdir /etc/holland/providers
-
-        insinto /etc/holland/backupsets
-        doins "${S}"/../../config/backupsets/examples/${PN##*-}.conf
-
-        insinto /etc/holland/providers
-        doins "${S}"/../../config/providers/${PN##*-}.conf
 }
