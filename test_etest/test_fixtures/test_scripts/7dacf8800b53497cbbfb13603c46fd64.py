c27d4a5e1a9ea4fb914a2eff19c8269f21d64a50
Alex Brandt <alunduil@alunduil.com>
2014-12-11 21:30:05 -0600

    add semicolon to unenclosed word exclusion characters
    Seems we need to ignore semicolons as word elements for everything to
    work correctly.  This adds a short test as well as the exclusion in the
    wondrous regex used for word tokenization.  This should fix #8.

---
+++ test_etest/test_fixtures/test_scripts/7dacf8800b53497cbbfb13603c46fd64.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2014 by Alex Brandt <alunduil@alunduil.com>
+#
+# etest is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from test_etest.test_fixtures.test_scripts import SCRIPTS
+
+_ = '''
+for test in test.py selftest.py selftest2.py; do
+	echo foo
+done
+'''  # flake8: noqa — inline bash script with tabs
+
+_ = {
+    'uuid': '7dacf8800b53497cbbfb13603c46fd64',
+
+    'description': 'for loop with unencapsulated word list',
+
+    'text': _,
+
+    'symbols': {},
+
+    'correct': None,
+}
+
+SCRIPTS.setdefault('all', []).append(_)
+SCRIPTS.setdefault('bash', []).append(_)
