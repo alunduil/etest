27a2aa5d4406478480f3064771c81f787c04c954
Alex Brandt <alunduil@alunduil.com>
2014-12-13 11:52:08 -0600

    Revert "Revert "remove volumes with docker containers""
    This reverts commit 1088f3d46a00fdb28ba7732cf8849b74a7065540.

--- etest/tests.py
+++ etest/tests.py
@@ -125,7 +125,7 @@ class Test(object):
             self.output += docker.container.logs(container_name).decode(encoding = 'utf-8')
 
             if self.failed:
-                docker.container.remove(container_name)
+                docker.container.remove(container_name, v = True)
                 self.failed_command = ' '.join(command)
                 break
 
@@ -140,7 +140,7 @@ class Test(object):
             image_name = self.name + ':' + tag_name
             image_names.append(image_name)
 
-            docker.container.remove(container_name)
+            docker.container.remove(container_name, v = True)
 
         for image_name in image_names:
             docker.image.remove(image_name)
