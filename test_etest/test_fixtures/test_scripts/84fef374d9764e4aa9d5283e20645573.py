0227e4062cb9ea5a9042cda956d4146af609d227
Alex Brandt <alunduil@alunduil.com>
2014-12-14 10:19:31 -0600

    add ! to accepted leading characters in word tokens
    We don't do any history analysis (not that I believe the grammar ever
    would) but we can accept ! as a legal character in a word for parsing
    purposes and handle other outcomes downstream.  This fixes #11.

---
+++ test_etest/test_fixtures/test_scripts/84fef374d9764e4aa9d5283e20645573.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2014 by Alex Brandt <alunduil@alunduil.com>
+#
+# etest is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from test_etest.test_fixtures.test_scripts import SCRIPTS
+
+_ = '''
+use !gtk3
+'''  # flake8: noqa — inline bash script with tabs
+
+_ = {
+    'uuid': 'd5aaeeee36574cd3bf4ffa100537703d',
+
+    'description': 'arguments to functions starting with bang',
+
+    'text': _,
+
+    'symbols': {
+    },
+
+    'correct': None,
+}
+
+SCRIPTS.setdefault('all', []).append(_)
+SCRIPTS.setdefault('bash', []).append(_)
