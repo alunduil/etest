11ae48ccd53906173e1e6c48535a9bac186181e9
Alex Brandt <alunduil@alunduil.com>
2014-12-27 21:00:59 -0600

    add + to legal word token start characters
    Fix #23 by adding + to the legal start characters for a word.

---
+++ test_etest/test_fixtures/test_scripts/04ec19a6834e4f4688a4e947a90826ce.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2014 by Alex Brandt <alunduil@alunduil.com>
+#
+# etest is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from test_etest.test_fixtures.test_scripts import SCRIPTS
+
+_ = '''
+find "${D}" -name '*.la' -exec rm -f '{}' +
+'''  # flake8: noqa — inline bash script with tabs
+
+_ = {
+    'uuid': '04ec19a6-834e-4f46-88a4-e947a90826ce',
+
+    'description': 'plus operator in find arguments',
+
+    'text': _,
+
+    'symbols': {
+    },
+
+    'correct': None,
+}
+
+SCRIPTS.setdefault('all', []).append(_)
+SCRIPTS.setdefault('bash', []).append(_)
