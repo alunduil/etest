41b56b001075fb8530df7c868eadd40965a823f3
Alex Brandt <alunduil@alunduil.com>
2014-12-28 19:08:34 -0600

    add asterisk to acceptable naked word tokens
    Fixes #27.

---
+++ test_etest/test_fixtures/test_scripts/88323eb04e6d47b89fd63988b618da67.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2014 by Alex Brandt <alunduil@alunduil.com>
+#
+# etest is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from test_etest.test_fixtures.test_scripts import SCRIPTS
+
+_ = '''
+dodoc -r *
+'''  # flake8: noqa — inline bash script with tabs
+
+_ = {
+    'uuid': '88323eb0-4e6d-47b8-9fd6-3988b618da67',
+
+    'description': 'asterisk word token',
+
+    'text': _,
+
+    'symbols': {
+    },
+
+    'correct': None,
+}
+
+SCRIPTS.setdefault('all', []).append(_)
+SCRIPTS.setdefault('bash', []).append(_)
