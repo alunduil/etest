1aee5d87fd41bb4ee662ad1554e55add03a4cf8e
Alex Brandt <alunduil@gmail.com>
2021-04-27 09:12:06 +0100

    Add stub build command for etest-build.
    We don't want to use a shell script to manage such a core piece of the
    packaging and want something mildly testable.  This stubs a python click
    command to build Docker images for etest that we can re-use in other
    places if desired and build a multitude of images (this could easily be
    reused in Google cloud builds for example.
    
    Note this is only a stub and the logic still needs to either be ported
    from the shell script or added.  Let me know if you'd like me to just
    add that functionality in this pull request (seems like a fine idea to
    me), but I will need review that I'm converting the logic correctly as
    BASH is finicky and quite easy to misinterpret.

---
+++ etest/build.py
@@ -0,0 +1,27 @@
+"""Build command for etest Docker images."""
+import os
+import sys
+from pathlib import Path
+from typing import List
+
+import click
+
+
+@click.command(name="etest-build")
+@click.option("--hardened/--no-hardened", default=False, help="Use a hardened profile.")
+@click.option("--multilib/--no-multilib", default=True, help="Use a multilib profile.")
+@click.option("--systemd/--no-systemd", default=False, help="Use a systemd profile.")
+# The following could and should be excised to an architecture module that
+# handles the nuances of architecture differences in Gentoo and Docker.
+@click.option(
+    "--architecture",
+    "--arch",
+    type=click.Choice(["amd64", "x86", "arm64", "armv5", "armv7", "ppc64"], case_sensitive=False),
+    default="amd64",
+    help="Architecture for the built image.",
+)
+@click.option("--environment", "--env", multiple=True, help="Pass an environment variable through to Docker.")
+@click.option("--name", type=Path, default=os.path.basename(sys.argv[0]), help="Name of the directory?")
+def main(hardened: bool, multilib: bool, systemd: bool, architecture: str, environment: List[str], name: Path) -> None:
+    """Build the etest image."""
+    pass
