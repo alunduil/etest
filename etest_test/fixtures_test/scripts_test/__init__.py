a9c89586b0d50e91786bdc98a65c1a02690f9817
alunduil <alunduil@gmail.com>
2022-12-08 13:28:29 +0000

    Convert bash parser tests to golden tests.

--- etest_test/fixtures_test/scripts_test/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Scripts Fixtures."""
-import os
-from typing import Any, Dict, List, Mapping
-
-from etest_test import helpers_test
-
-SCRIPTS: Dict[str, List[Mapping[str, Any]]] = {}
-
-helpers_test.import_directory(__name__, os.path.dirname(__file__))
---
+++ etest_test/parsers_test/bash_test.py
@@ -0,0 +1,39 @@
+"""Bash Parser Tests."""
+import logging
+
+import pytest
+import pytest_golden
+
+import etest.lexers.bash as _lexer
+import etest.parsers.bash as sut
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.golden_test("bash_test_fixtures/correct_*.yaml")  # type: ignore[misc]
+def test_golden_bash_correct_parses(
+    lexer: _lexer.BashLexer, golden: pytest_golden.plugin.GoldenTestFixture
+) -> None:
+    """Test golden corrects in BashParser."""
+    parser = sut.BashParser()
+    parser.build(debug=1, debuglog=_LOGGER)
+
+    assert parser.parser is not None  # nosec
+
+    parser.parser.parse(debug=_LOGGER, input=golden["text"], lexer=lexer)
+
+    assert parser.symbols == golden.out["symbols"]  # nosec
+
+
+@pytest.mark.golden_test("bash_test_fixtures/error_*.yaml")  # type: ignore[misc]
+def test_golden_bash_error_parses(
+    lexer: _lexer.BashLexer, golden: pytest_golden.plugin.GoldenTestFixture
+) -> None:
+    """Test golden errors in BashParser."""
+    parser = sut.BashParser()
+    parser.build(debug=1, debuglog=_LOGGER)
+
+    assert parser.parser is not None  # nosec
+
+    with pytest.raises(sut.BashSyntaxError):
+        parser.parser.parse(debug=_LOGGER, input=golden["text"], lexer=lexer)
